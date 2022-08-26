0be7a30a094576ba5e2cbc83cfa0ae1bc3cb7880
Manasseh <91884645+ManassehGit@users.noreply.github.com>
2022-08-27 00:47:06 +0100

    Add top navigation component

---
+++ src/components/Dashboard/Topnav/Topnav.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Topnav = () => {
+  return (
+    <nav className="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+        <div className="container-fluid"><button className="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i className="fas fa-bars"></i></button>
+                        
+            <ul className="nav navbar-nav flex-nowrap ml-auto">
+                <div className="d-none d-sm-block topbar-divider"></div>
+                    <li className="nav-item dropdown no-arrow" role="presentation">
+                        <div className="nav-item dropdown no-arrow"><Link className="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" to="#"><span className="d-none d-lg-inline mr-2 text-gray-600 small">IIOT Test</span></Link>
+                            <div className="dropdown-menu shadow dropdown-menu-right animated--grow-in" role="menu">
+                                    <Link className="dropdown-item" role="presentation" to="/"><i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Profile</Link>
+                                    <div className="dropdown-divider"></div>
+                                    <Link className="dropdown-item" role="presentation" to="index"><i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Logout</Link></div>
+                            </div>
+                    </li>
+            </ul>
+        </div>
+    </nav>  
+  )
+}
+
+export default Topnav
+

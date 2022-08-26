9fc603923276e1ef5076fe02241e03ccccfd983c
Manasseh <91884645+ManassehGit@users.noreply.github.com>
2022-08-26 07:14:13 +0100

    Add base form for broker connection

--- src/components/Settings/ConfigBroker.jsx
+++ src/components/Settings/ConfigBroker.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const ConfigBroker = () => {
   return (
-    <div>ConfigBroker</div>
+    <>
+        <div className='container m-4'>
+        <form>
+            <div class="form-group row">
+                <label for="port" class="col-sm-2 col-form-label">Port</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="port" placeholder="Port Number" />
+                </div>
+            </div>
+            <div class="form-group row mt-2">
+                <label for="other" class="col-sm-2 col-form-label">Other</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="other" placeholder="Other" />
+                </div>
+            </div>
+        </form>
+        </div>
+    </>
   )
 }
 

a45c49bf8a570f2c6adcf4c2b834f23d69e6566b
Manasseh <91884645+ManassehGit@users.noreply.github.com>
2022-08-26 17:35:40 +0100

    Ready for dashboard

--- src/components/Settings/ConfigPubLimit.jsx
+++ src/components/Settings/ConfigPubLimit.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const ConfigPubLimit = () => {
   return (
-    <div>ConfigPubLimit</div>
+    <div className='container m-4'>
+        <form>
+            <div class="form-group row">
+                <label for="publisher" class="col-sm-2 col-form-label">publisher</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="publisher" placeholder="publisher Number" />
+                </div>
+            </div>
+            <div class="form-group row mt-2">
+                <label for="other" class="col-sm-2 col-form-label">Other</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="other" placeholder="Other" />
+                </div>
+            </div>
+        </form>
+        </div>
   )
 }
 

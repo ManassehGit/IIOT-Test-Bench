94c79be691753182b775ab8720972b33b7c5726b
Manasseh <91884645+ManassehGit@users.noreply.github.com>
2022-08-27 02:22:14 +0100

    Add index page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ConfigConn from './components/Dashboard/ConfigureConnection/ConfigConn';
 import DashBase from './components/Dashboard/DashBase/DashBase';
+import IndexPage from './components/IndexPage/IndexPage';
 import ConfigBroker from './components/Settings/ConfigBroker';
-import Settings from './components/Settings/Settings';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Settings />}/>
+        <Route path="/" element={<IndexPage />}/>
         <Route path="configbroker" element={<ConfigBroker />}/>
         <Route path="dash" element={<DashBase />}/>
         <Route path="configconn" element={<ConfigConn />}/>

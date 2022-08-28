7922878a98cf98ad8564dbbfdf6183132bf6851a
Manasseh <91884645+ManassehGit@users.noreply.github.com>
2022-08-28 09:49:32 +0100

    Create generate client ID helper function

---
+++ src/modules/HelperFunctions/generateClientId.js
@@ -0,0 +1,10 @@
+//Function to generate random client Id for users that would select option
+const generateID = () => {
+    let id = "user1234";
+    //using a sample string 
+
+
+    return id;
+}
+
+export default generateID;
